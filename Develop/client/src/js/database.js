c84391bf1346605054d242c3390e6a2652d766a9
Durgavino <mailtodurga93@gmail.com>
2023-03-20 14:01:48 -0300

    updated the Put

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,7 +13,14 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id,content) => {console.error('putDb not implemented');
+const txtEditDb=await openDB('jate',1);
+const tx=txtEditDb.transaction('jate','readwrite');
+const store=tx.objectStore('jate');
+const request=store.put({id:id,jate:content});
+const result=await request;
+console.log('Data saved to the Database',result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
@@ -26,5 +33,5 @@ const result=await request;
 console.log('result.value',result
 );
 return result;
-}
+};
 initdb();
